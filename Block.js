5707105287b2236102e98be4318a5a2199690fb9
Vishv2912 <81970918+Vishv2912@users.noreply.github.com>
2021-05-21 16:19:01 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,40 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.image=loadImage("block.png")
+
+      this.visibility=255;
+
+
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+
+    }
+    display(){
+
+
+      if(this.body.speed<5){
+  var pos= this.body.position;
+  
+  rectMode(CENTER);
+  rect(pos.x,pos.y,this.width, this.height);
+
+
+  
+}else{
+
+  World.remove(world,this.body);
+
+  push();
+  tint(255,this.visibility);
+  this.visibility=this.visibility-5
+  image(this.image,this.body.x-18.5,this.body.y-23);
+
+  pop();    
+}
+}}
